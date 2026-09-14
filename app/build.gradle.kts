859997af68a35b4aea535f3ada7ff179049a9997
sampple-korea <root@lvl.kr>
2026-09-15 06:37:46 +0900

    release: prepare limited Korean USB audio alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,7 +114,7 @@ android {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.0-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
