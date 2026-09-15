adb7a3a4f789a3c69b81053d165c6dcb9980412c
sampple-korea <root@lvl.kr>
2026-09-16 03:28:45 +0900

    chore(release): prepare UAC2 control and APK import alpha 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,8 +113,8 @@ android {
         applicationId = "com.virtualdap.host"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.0-alpha.2"
+        versionCode = 3
+        versionName = "0.1.0-alpha.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
